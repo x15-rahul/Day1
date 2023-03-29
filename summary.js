c8b0e116090048a78ef95dd9d01f530ce837f28a
x15-rahul <biswasrahul97@outlook.com>
2023-03-29 12:53:39 +0600

    summary file added

---
+++ summary.js
@@ -0,0 +1,30 @@
+// working with string
+const myName = 'Rahul Biswas';
+const myString = 'What the fuck you are doing! What the hell';
+const nickName = ' Sumit ';
+// console.log(myName.indexOf('Bis'))
+// console.log(myName.includes('Biswas'));
+// for(const char of myName){
+//     console.log(char);
+// }
+
+// console.log(myName.concat(' ', nickName));
+// console.log(myName.toUpperCase());
+// const replacedWord = myString.replaceAll('the', 'Sumit');
+// console.log(replacedWord);
+// console.log(myName);
+// const slicedWord = myName.slice(0, 5);
+// console.log(slicedWord);
+// console.log(myName);
+
+// const splitedWord = myString.split(' ');
+// // console.log(splitedWord.join('|'));
+// console.log(splitedWord.join('|'));
+
+// console.log(myName.endsWith('was'));
+// console.log(myString.lastIndexOf('the'));
+// console.log(nickName);
+// console.log(nickName.trim());
+
+console.log(myName.substring(0, 5));
+console.log(myName.slice(0, 5));
